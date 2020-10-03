c73edfb39f44ae4ba49e4d79ed7d914865f358a0
Edvinas <edvinas2121@hotmail.com>
2020-10-03 21:33:58 +0300

    user can select delimiter, column count validation

--- app/controllers/CsvController.php
+++ app/controllers/CsvController.php
@@ -10,17 +10,19 @@ class CsvController extends Controller{
         try {
             $csv = $_FILES['csv'];
             $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
-
             if($csv['size']>0 && in_array($csv['type'],$csvMimes) && strlen($csv['name'])<=70){
                 $tableName = $csv['name'].date("Y/m/d_h:i:s");
                 $csvModel = $this->model('Csv');
-                $data= $csvModel->parseCsvData($csv);
-                $csvModel->createCsvTable($tableName,count($data[0]));
+                $data= $csvModel->parseCsvData($csv,$_POST['delimiter']);
+                $colCount = count($data[0]);
+                if($colCount <= 0 && $colCount >= 16384){
+                    throw new Exception("Incorrect column count");
+                };
+                $csvModel->createCsvTable($tableName,$colCount);
                 $csvModel->insertCsvTable($tableName,$data); 
                 $csvModel->updateTableList($tableName); 
-
             }else{
-                echo $this->jsonException('file must be csv type and file name can\'t be longer than 70 characters');
+                echo $this->jsonException('File must be csv type, can\'t be empty and file name can\'t be longer than 70 characters');
             }
         } catch (Exception $exception) {
             echo $this->jsonException($exception->getMessage());
--- app/models/Csv.php
+++ app/models/Csv.php
@@ -2,7 +2,8 @@
 
 class Csv extends Database{
 
-    public function parseCsvData(array $csv){
+    public function parseCsvData(array $csv, $delimiter){
+        $delimiterArr = ["comma"=>",","semicolon"=>";","pipe"=>"|"];
         //SET USER SELECTED DELIMETER
         $fileName = $csv['tmp_name'];
         $file = fopen($fileName,"r");
@@ -16,10 +17,13 @@ class Csv extends Database{
 
     public function createCsvTable(string $tableName,int $ColumnCount){
         $query = $this->buildCreateQuery($tableName,$ColumnCount);
+        echo $query;
         mysqli_query($this->getDbConnection(),$query);
     }
     public function insertCsvTable(string $tableName,array $data){
         $query = $this->buildInsertQuery($tableName,$data);
+        echo $query;
+
         mysqli_query($this->getDbConnection(),$query);
 
     }
@@ -31,25 +35,23 @@ class Csv extends Database{
             $query = "SELECT * FROM `tables_list` WHERE `new` = '$oldTable'";
         }else{
             $newFileQuery = "INSERT INTO `tables_list` (new) VALUES ('$newTable');";
-            echo $newFileQuery;
             mysqli_query($this->getDbConnection(),$newFileQuery);
         }
     }
-    protected function buildCreateQuery(string $tableName,int $ColumnCount){
-        $columnNameArr = range('A','Z');
+    protected function buildCreateQuery(string $tableName,int $columnCount){
+        $columnNameArr = range(1,$columnCount);
         $columns = '';
-        for($i=0; $i<= $ColumnCount-1; $i++){
-            $columns .= $columnNameArr[$i] . " VARCHAR(200)";
-            $columns .= $i === $ColumnCount-1 ? "" : ",";
+        for($i=0; $i<= $columnCount-1; $i++){
+            $columns .= "`$columnNameArr[$i]`" . " VARCHAR(200)";
+            $columns .= $i === $columnCount-1 ? "" : ",";
         }
         return "CREATE TABLE `$tableName` ($columns);";
     }
     protected function buildInsertQuery(string $tableName,array $data){
-        $columnNameArr = range('A','Z');
         $columnCount = count($data[0]);
         $rowCount = count($data);
-        $columnNames = implode(",",array_slice($columnNameArr,0,$columnCount));
-        $query = "INSERT INTO `$tableName` ($columnNames) VALUES ";
+        $columnNames = implode("`,`",range(1,$columnCount));
+        $query = "INSERT INTO `$tableName` (`$columnNames`) VALUES ";
         for ($i=0; $i <=$rowCount-1 ; $i++) { 
             $row = implode('\',\'',$data[$i]);
             $query .= "('$row')" . ($rowCount-1 !== $i ? ',':';');
